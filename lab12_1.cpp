c24b2df39065621a9042f3b1a5b75ea3ff318d50
Panyapat Wongdee <panyapat12345@gmail.com>
2021-12-21 16:35:22 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,32 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    
+    srand(time(0));
+    string grade;
+    int i = rand()%9;
+
+    switch(i){
+        case 0:     grade = "A";    break;
+        case 1:     grade = "B+";   break;
+        case 2:     grade = "B";    break;
+        case 3:     grade = "C+";   break;
+        case 4:     grade = "C";    break;
+        case 5:     grade = "D+";   break;
+        case 6:     grade = "D";    break;
+        case 7:     grade = "F";    break;
+        case 8:     grade = "W";    break;
+    }
+    
+    cout << "You will get " << grade << " in this 261102.";
+
+    return 0;
+}
